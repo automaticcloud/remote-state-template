e5db956cead817ef4336eb5fdc04701389bcc06b
Kiran Patel <kiran.patel@automaticcloud.co.uk>
2023-10-02 13:37:54 +0100

    format fixes

--- tfstate.tf
+++ tfstate.tf
@@ -12,17 +12,17 @@ resource "aws_s3_bucket" "terraform_state" {
 
 resource "aws_s3_bucket_versioning" "terraform_state" {
   bucket = aws_s3_bucket.terraform_state.id
-  
+
   versioning_configuration {
-     status = "Enabled"
+    status = "Enabled"
   }
 }
 
 resource "aws_dynamodb_table" "terraform_state_lock" {
-  name = "state"
-  read_capacity = 1
+  name           = "state"
+  read_capacity  = 1
   write_capacity = 1
-  hash_key = "LockID"
+  hash_key       = "LockID"
 
   attribute {
     name = "LockID"
